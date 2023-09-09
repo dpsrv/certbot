a497e97225314ad411162a01b4fdb42b71edfe39
Max <max@maxf.net>
2023-09-09 00:22:47 -0400

    updated

--- certonly.sh
+++ certonly.sh
@@ -7,3 +7,8 @@ cd -
 domains=$($WD/getDomains.sh /etc/bind/zones|tr '\n' ','|sed 's/,$//g')
 
 certbot certonly -n --agree-tos -m $LETSENCRYPT_EMAIL --webroot -w /var/www/ -d "$domains"
+
+domain=${domains%%,*}
+
+[ ! -e /etc/letsencrypt/live/domain ] || rm  /etc/letsencrypt/live/domain
+ln -s /etc/letsencrypt/live/${domain%%,*} /etc/letsencrypt/live/domain
