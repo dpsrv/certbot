9a0fce2a0363e428a9e4e596749d04123c60bf7f
Maxim Fortun <max@maxf.net>
2023-12-05 14:15:59 -0500

    updated

--- certonly.sh
+++ certonly.sh
@@ -6,7 +6,7 @@ cd -
 
 domains=$($WD/getDomains.sh /etc/bind/zones|tr '\n' ','|sed 's/,$//g')
 
-certbot certonly -n --agree-tos -m $LETSENCRYPT_EMAIL --webroot -w /var/www/ -d "$domains"
+certbot certonly -n --agree-tos -m $LETSENCRYPT_EMAIL --webroot -w /var/www/ -d "$domains" --expand "$@"
 
 domain=${domains%%,*}
 
